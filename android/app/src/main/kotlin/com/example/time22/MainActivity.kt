26844b48f82d734910b512efbbcf02bb8086435f
gyroman2012 <44126630+gyroman2012@users.noreply.github.com>
2022-07-23 19:01:22 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/time22/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.time22
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,201 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double value = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                colors: [
+                  Colors.purple,
+                  Colors.blue,
+                ],
+              ),
+            ),
+          ),
+          SafeArea(
+            child: Container(
+              width: 200.0,
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  DrawerHeader(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CircleAvatar(
+                          radius: 50.0,
+                          child: Image.asset(
+                            'assets/images/download.jpg',
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Text(
+                          'David Dyson',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: ListView(
+                      children: [
+                        ListTile(
+                          onTap: () {},
+                          leading: Icon(
+                            Icons.home,
+                            color: Colors.white,
+                          ),
+                          title: Text(
+                            'Home',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        ListTile(
+                          onTap: () {},
+                          leading: Icon(
+                            Icons.person,
+                            color: Colors.white,
+                          ),
+                          title: Text(
+                            'Profile',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        ListTile(
+                          onTap: () {},
+                          leading: Icon(
+                            Icons.settings,
+                            color: Colors.white,
+                          ),
+                          title: Text(
+                            'Settings',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        ListTile(
+                          onTap: () {},
+                          leading: Icon(
+                            Icons.logout,
+                            color: Colors.white,
+                          ),
+                          title: Text(
+                            'Log out',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          TweenAnimationBuilder(
+            tween: Tween<double>(begin: 0, end: value),
+            duration: Duration(milliseconds: 500),
+            builder: (_, double val, __) {
+              return (Transform(
+                alignment: Alignment.center,
+                transform: Matrix4.identity()
+                  ..setEntry(3, 2, 0.001)
+                  ..setEntry(0, 3, 200 * val)
+                  ..rotateY((pi / 6) * val),
+                child: Scaffold(
+                  backgroundColor: Colors.grey,
+                  appBar: AppBar(
+                    title: Text('3D Drawer Menu'),
+                  ),
+                  body: Center(
+                    child: Column(
+                      children: [
+                        SizedBox(height: 200),
+                        Text('Swipe right to open menu.'),
+                        SizedBox(height: 100),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Text('Poke Me!'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ));
+            },
+          ),
+          GestureDetector(
+            onHorizontalDragUpdate: (e) {
+              if (e.delta.dx > 0) {
+                setState(() {
+                  value = 1;
+                });
+              } else {
+                setState(() {
+                  value = 0;
+                });
+              }
+              ;
+            },
+          ),
+          // GestureDetector(onTap: () {
+          // setState(
+          //   () {
+          //    value == 0 ? value = 1 : value = 0;
+          //  },
+          //  );
+          // }),
+        ],
+      ),
+    );
+  }
+}
